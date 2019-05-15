40b49cb8d216561abe2ee2502da41233eb7ee7f5
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-15 18:08:08 +0530

    Added player movement

--- Sweetest Dream/Assets/Scripts/Player/PlayerMovement.cs
+++ Sweetest Dream/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,56 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    private Vector3 _movement;
+    private Animator _anim;
+    private Rigidbody _playerRb;
+    private int _floorMask;
+    private float _camRayLength = 100f;
+
+    void Awake()
+    {
+        _floorMask = LayerMask.GetMask("Floor");
+        _anim = GetComponent<Animator>();
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turn();
+        Animate(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        _movement.Set(h, 0f, v);
+        _movement = _movement.normalized * speed * Time.deltaTime;
+        _playerRb.MovePosition(transform.position + _movement);
+    }
+
+    void Turn()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, _camRayLength, _floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            _playerRb.MoveRotation(newRotation);
+        }
+    }
+
+    void Animate(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        _anim.SetBool("IsWalking", walking);
+    }
 }
